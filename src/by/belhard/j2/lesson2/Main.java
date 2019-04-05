f51f8a5245086d575365e7c09e26a1c2c80f2f65
Galdava <alexgaldava@gmail.com>
2019-04-05 21:29:48 +0300

    1

--- src/by/belhard/j2/lesson2/Main.java
+++ src/by/belhard/j2/lesson2/Main.java
@@ -1,9 +1,32 @@
 package by.belhard.j2.lesson2;
 
 public class Main {
-    int x;
+
 
     public static void main(String[] args) {
 
+        int x = 24;
+       /* if (x == 20) {
+            x--;
+        } else if (x == 21){
+            x++;
+        } else {
+            x /= 2;
+        }*/
+
+       // int y = x%2==0 ? x/2 : -x;
+
+       // System.out.println(x);
+       // System.out.println(x%2==0 ? x/2 : -x);
+       /* do {
+            System.out.println(x);
+            x--;
+        } while (x == 15);
+        System.out.println("lol");*/
+        for (int i =4; i < 10; i++){
+        System.out.println(i);}
+        System.out.println("uhu");
+
     }
+
 }
